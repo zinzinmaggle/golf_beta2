e7972b673bed09fd50799ee333874c3484237f90
na-ji <naji.demolitionman@gmail.com>
2015-05-13 11:50:37 +0200

    Update login form

---
+++ client/templates/application/login.js
@@ -0,0 +1,3 @@
+Template._loginButtonsLoggedOutWithoutDropdown.replaces("_loginButtonsLoggedOutDropdown");
+Template._loginButtonsLoggedOutWithoutDropdown.inheritsHelpersFrom("_loginButtonsLoggedOutDropdown");
+Template._loginButtonsLoggedOutWithoutDropdown.inheritsEventsFrom("_loginButtonsLoggedOutDropdown");
