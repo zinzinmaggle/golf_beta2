05dba7ed070e59e13423a749ae7d48d716b4a8a4
zinzinmaggle <frederic.quemper@edu.esiee.fr>
2015-05-18 10:41:59 +0200

    Edit Login Form 3

--- client/templates/connexion/login.js
+++ client/templates/connexion/login.js
@@ -11,3 +11,24 @@ Template._loginButtonsLoggedOutWithoutDropdown.inheritsEventsFrom("_loginButtons
 Template.slider_connexion.rendered = function () {
 $('.carousel').carousel()
 };
+
+/*************************************Connexion************************************************/
+
+/*************************************Inscription***********************************************/
+Template.inscription.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    if($(e.target).find('[name=username]').val() === "username")
+    {
+    	alert("coucou");
+    }
+	/*Meteor.users.update({_id:Meteor.user()._id}, {$set: { surname: $(e.target).find('[name=username]').val()
+	firstname: $(e.target).find('[name=fname]').val(),
+	lastname: $(e.target).find('[name=lname]').val(),
+    level : 'beginner',
+    accounttype : 'regular'
+	}});*/
+    
+  }
+});
