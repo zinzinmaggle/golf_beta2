a373893cec9c2325f411a4985da90b990169409d
zinzinmaggle <frederic.quemper@edu.esiee.fr>
2015-05-19 16:44:05 +0200

    Edit loginForm 4

--- client/templates/connexion/loginForm.js
+++ client/templates/connexion/loginForm.js
@@ -2,7 +2,50 @@ Template.headerloginForm.helpers({
 	title: function(){
 		if (Router.current().route.path(this) === '/login')
 			return 'Connexion';
-		else
+		else if(Router.current().route.path(this) === '/register')
 			return 'Inscription';
+		else if(Router.current().route.path(this) === '/forgotPassword')
+			return 'Mot de passe oublié';
+		else
+			return 'Déconnecté';
 	}
 });
+Template.login.rendered = function () {
+
+   var a = document.getElementById("at-forgotPwd");
+   a.href = "forgotPassword";
+ 
+};
+
+AccountsTemplates.configure({
+    // Behaviour
+    confirmPassword: true,
+    enablePasswordChange: true,
+    forbidClientAccountCreation: true,
+    overrideLoginErrors: true,
+    sendVerificationEmail: false,
+    lowercaseUsername: false,
+
+    // Appearance
+    showAddRemoveServices: true,
+    showForgotPasswordLink: true,
+    showLabels: true,
+    showPlaceholders: true,
+    showResendVerificationEmailLink: false,
+
+    hideSignInLink:true,
+    hideSignUpLink:true,
+
+    homeRoutePath: '/',
+    redirectTimeout: 4000,
+    
+     texts: {
+       pwdLink_link: "forgotPassword",
+       socialSignUp: "Register",
+       socialIcons: {
+          "meteor-developer": "fa fa-rocket"
+      },
+    },
+    
+});
+
--- server/publication_photo_slider.js
+++
@@ -1,3 +0,0 @@
-Meteor.publish('photos', function() {
-  return Photos.find();
-});
