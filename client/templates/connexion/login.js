65a6178a05bece059d683d413daf76de9aec5ac4
zinzinmaggle <frederic.quemper@edu.esiee.fr>
2015-05-19 17:38:10 +0200

    Edit loginForm 5

--- client/templates/connexion/login.js
+++ client/templates/connexion/login.js
@@ -1,3 +1,9 @@
+/*Template.atForm.rendered = function () {
+
+   	var a = document.getElementById("at-forgotPwd");
+	a.href = "/fgtPassword";
+
+};*/
 Template.slider_connexion.rendered = function () {
     $('.carousel').carousel()
 };
--- client/templates/connexion/loginForm.js
+++ client/templates/connexion/loginForm.js
@@ -4,18 +4,13 @@ Template.headerloginForm.helpers({
 			return 'Connexion';
 		else if(Router.current().route.path(this) === '/register')
 			return 'Inscription';
-		else if(Router.current().route.path(this) === '/forgotPassword')
+		else if(Router.current().route.path(this) === '/fgtPassword')
 			return 'Mot de passe oublié';
 		else
 			return 'Déconnecté';
 	}
 });
-Template.login.rendered = function () {
 
-   var a = document.getElementById("at-forgotPwd");
-   a.href = "forgotPassword";
- 
-};
 
 AccountsTemplates.configure({
     // Behaviour
@@ -35,17 +30,11 @@ AccountsTemplates.configure({
 
     hideSignInLink:true,
     hideSignUpLink:true,
-
+    
     homeRoutePath: '/',
     redirectTimeout: 4000,
     
-     texts: {
-       pwdLink_link: "forgotPassword",
-       socialSignUp: "Register",
-       socialIcons: {
-          "meteor-developer": "fa fa-rocket"
-      },
-    },
+    
     
 });
 
