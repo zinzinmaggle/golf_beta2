55b113e3a402dab452fd5ab42307e383ff9699e5
na-ji <naji.demolitionman@gmail.com>
2015-05-20 16:59:02 +0200

    Fix fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -12,11 +12,15 @@ if (Posts.find().count() === 0) {
 if (Region.find().count() === 0) {
 	var dbRegion = [];
 	dbRegion = JSON.parse(Assets.getText("assets/region.json"));
-	Region.insert(dbRegion);
+	for (var i = dbRegion.length - 1; i >= 0; i--) {
+		Region.insert(dbRegion[i]);
+	};
 }
 
 if (Golf.find().count() === 0) {
 	var dbGolf = [];
 	dbGolf = JSON.parse(Assets.getText("assets/golf.json"));
-	Golf.insert(dbGolf);
+	for (var i = dbGolf.length - 1; i >= 0; i--) {
+		Golf.insert(dbGolf[i]);		
+	};
 }
