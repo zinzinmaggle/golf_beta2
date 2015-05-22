7eb6bff9e9889ce1d9f6114e15694533b2535b7e
na-ji <naji.demolitionman@gmail.com>
2015-05-22 10:40:16 +0200

    Add result number in search

--- client/templates/Golf/golf_search.js
+++ client/templates/Golf/golf_search.js
@@ -1,5 +1,24 @@
+var instance;
+
+Template.golfSearch.rendered = function() {
+    instance = EasySearch.getComponentInstance({
+        id: 'golfSearch',
+        index: 'golfs'
+    });
+};
+
 Template.golfSearch.helpers({
 	golfs: function() {
 		return Golf.find();
+	},
+	results: function(){
+		if (instance !== undefined)
+			return instance.get('total');
+		return false;
+	},
+	pluralize: function() {
+		if (instance !== undefined && instance.get('total') > 1)
+			return 's';
+		return '';
 	}
 });
