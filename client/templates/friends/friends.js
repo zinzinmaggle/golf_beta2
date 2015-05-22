b1d7df5d49688adb211cb87206abf4150cceb40a
na-ji <naji.demolitionman@gmail.com>
2015-05-22 18:57:04 +0200

    Update friend list

--- client/templates/friends/friends.js
+++ client/templates/friends/friends.js
@@ -1,86 +1,92 @@
 Template.showFriendsQuery.helpers({
-  friendsQuery: function() {
-    return Friends.find({ accepted: false, id2 : Meteor.userId() }).fetch();
-  }
-  
+	friendsQuery: function() {
+		return Friends.find({ accepted: false, id2 : Meteor.userId() }).fetch();
+	}
 });
+
 Template.showFriendsList.helpers({
-  friendsList: function() {
-  	var frd = Friends.find({$or: [{ accepted: true, id2 : Meteor.userId() }, { accepted: true, id1 : Meteor.userId() }]}).fetch(),
-  		friends =[],
-      user;
+	friendsList: function() {
+		var frd = Friends.find({$or: [{ accepted: true, id2 : Meteor.userId() }, { accepted: true, id1 : Meteor.userId() }]}).fetch(),
+			friends =[],
+			user;
 
-  	for (var i = frd.length - 1; i >= 0; i--) {
-  		var id;
-  		if (frd[i].id1 == Meteor.userId()) {
-  			id = frd[i].id2;
-  		} else {
-  			id = frd[i].id1;
-  		}
-  		user = Meteor.users.find({_id: id}).fetch();
-  		
-  		friends.push(user[0]);
-  	};
+		for (var i = frd.length - 1; i >= 0; i--) {
+			var id;
+			if (frd[i].id1 == Meteor.userId()) {
+				id = frd[i].id2;
+			} else {
+				id = frd[i].id1;
+			}
+			user = Meteor.users.find({_id: id}).fetch();
+			
+			friends.push(user[0]);
+		};
 
-  	console.log(friends);
-
-    return friends;
- }
+		return friends;
+	}
 });
+
 Template.showFriends.events({
-  'click .btn': function (event) {
-    event.preventDefault();
+	'click .btn': function (event) {
+		event.preventDefault();
 
-    var $button = $(event.target);
+		var $button = $(event.target);
 
-   	Friends.update({
-		_id: $button.attr('user-id')
-	}, {
-		$set: { 
-			accepted: true
-		}
-	});
- 	
- 	Meteor.users.insert({
-	 	friends: {
-	        friend : Friends.find({ accepted: true, id2 : Meteor.userId() }).username             
-	    },
-	});
-  }
+		Friends.update({
+			_id: $button.attr('user-id')
+		}, {
+			$set: { 
+				accepted: true
+			}
+		});
+	
+		Meteor.users.insert({
+			friends: {
+						friend : Friends.find({ accepted: true, id2 : Meteor.userId() }).username             
+				},
+		});
+	},
+	'click paper-tab': function (e) {
+		console.log($(e.target).attr('page'));
+		Session.set('currentTab', $(e.target).attr('page'));
+		// $('core-pages#tabContent').attr('selected', function() {
+		// 	return $(e.target).attr('page')
+		// });
+	},
 });
 
 Template.showFriends.helpers({
-  friendQueryCounter: function() {
-    var Counter = Friends.find({ accepted: false, id2 : Meteor.userId() }).count();
-    if(Counter === 0)
-    {
-      return '';
-    }
-    else
-    {
-      return "("+Counter+")";
-    }
-  },
-  friendListCounter: function() {
-    var frd = Friends.find({$or: [{ accepted: true, id2 : Meteor.userId() }, { accepted: true, id1 : Meteor.userId() }]}).fetch(),
-      friends = [],
-      user;
+	friendQueryCounter: function() {
+		var Counter = Friends.find({ accepted: false, id2 : Meteor.userId() }).count();
+		if(Counter === 0)
+		{
+			return '';
+		}
+		else
+		{
+			return "("+Counter+")";
+		}
+	},
+	friendListCounter: function() {
+		var frd = Friends.find({$or: [{ accepted: true, id2 : Meteor.userId() }, { accepted: true, id1 : Meteor.userId() }]}).fetch(),
+			friends = [],
+			user;
 
-    for (var i = frd.length - 1; i >= 0; i--) {
-      var id;
-      if (frd[i].id1 == Meteor.userId()) {
-        id = frd[i].id2;
-      } else {
-        id = frd[i].id1;
-      }
-      user = Meteor.users.find({_id: id}).fetch();
-      
-      friends.push(user[0]);
-    };
+		for (var i = frd.length - 1; i >= 0; i--) {
+			var id;
+			if (frd[i].id1 == Meteor.userId()) {
+				id = frd[i].id2;
+			} else {
+				id = frd[i].id1;
+			}
+			user = Meteor.users.find({_id: id}).fetch();
+			
+			friends.push(user[0]);
+		};
 
-    console.log(friends);
-
-    return "("+friends.length+")";
- }
-
+		return "("+friends.length+")";
+	},
+	currentTab: function() {
+		return Session.get('currentTab');
+	}
 });
