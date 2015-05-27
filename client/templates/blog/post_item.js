22f4ff72a552f5a8eecae83ce4c810efa0c8516d
zinzinmaggle <frederic.quemper@edu.esiee.fr>
2015-05-28 01:29:05 +0200

    Blog Edit

--- client/templates/blog/post_item.js
+++ client/templates/blog/post_item.js
@@ -1,9 +1,57 @@
 Template.postItem.helpers({
-  domain: function() {
+  date: function() {
 
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
+  	var ma_date = new Date();
+  	var time = Date.parse(ma_date)-Date.parse(this.submit_date);
+  	var d, h, m, s;
+	  s = Math.floor(time / 1000);
+	  m = Math.floor(s / 60);
+	  s = s % 60;
+	  h = Math.floor(m / 60);
+	  m = m % 60;
+	  d = Math.floor(h / 24);
+	  h = h % 24;
+	  var custom_date;
+
+	  
+	  if(s>0 && m==0 && h ==0 && d==0)
+	  {
+	  	custom_date = s+'s';
+	  }
+	  else if(s>0 && m>0 && h == 0 && d== 0)
+	  {
+	 	
+	 	if(m>1)
+	 	{
+	 		custom_date = m+'mins';
+	 	}
+	 	else
+	 	{
+	 		custom_date = m+'min';
+	 	}
+	  }
+	  else if(s>0 && m>0 && h>0 && d==0)
+	  {
+	  	custom_date =  h+'h';
+	  }
+	  else if(s>0 && m>0 && h>0 && d>0)
+	  {
+	  	
+	  	if(d>1)
+	  	{
+	  		custom_date = d+' jrs';
+	  	}
+	  	else
+	  	{
+	  		custom_date = d+' jr';
+	  	}
+	  }
+	  else
+	  {
+	  	custom_date = "now"; 
+	  }
+  	
+	  return custom_date;
   }
 });
 
