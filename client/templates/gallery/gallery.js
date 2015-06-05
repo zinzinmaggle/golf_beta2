0df3dbb116196f26bb51cbaa91e8ff907b8557d4
zinzinmaggle <frederic.quemper@edu.esiee.fr>
2015-06-06 01:09:12 +0200

    Edit groups js

--- client/templates/gallery/gallery.js
+++ client/templates/gallery/gallery.js
@@ -1,24 +1,37 @@
-var isError = 0;
+var ImageTab =[];
 Template.gallery.rendered = function() {
        Session.set('currentTab', 'tab1');
        if(Meteor.isClient){
        Dropzone.autoDiscover = false;
-       var ImageTab =[];
-       var dropzone = new Dropzone("div#test",{url : '/gallery/'});
+       
+       var dropzone = new Dropzone(".dropzone",{
+        url : '127.0.0.1/upload.php',
+      });
       
           
 
        dropzone.on("complete",function(file){
-          ImageTab.push(file.name);
-          Meteor.call("Insert", ImageTab, Meteor.userId(),'0');
+        
+          if (file.type != "image/jpeg" && file.type != "image/png") {
+          
+          }
+    
        });  
        
+       dropzone.on("success",function(file,ServerMessage){
 
+
+         ImageTab.push(file.name);
+        
+        
+
+       });  
 
 
 
        dropzone.on("error",function(file,errorMessage){
-              isError = 1;
+              
+       
        });  
 
        /*faire un event sur erreur qui disabled le bouton*/
@@ -29,22 +42,14 @@ Template.gallery.events({
   'click paper-tab': function (e) {
     console.log($(e.target).attr('page'));
     Session.set('currentTab', $(e.target).attr('page'));
-    // $('core-pages#tabContent').attr('selected', function() {
-    //  return $(e.target).attr('page')
-    // });
+    
   },
   'click .creeralbum':function(e){
     e.preventDefault();
-    if($(e.target).find('[name=nalbum]').val() == "" || isError == 1)
-    {
-      alert('entrer un nom d\'album');
-    }
-   
-    else
-    {
-      Meteor.call('InsertDefinitive', $(e.target).find('[name=nalbum]').val(),'1',Meteor.userId());
+    console.log($(e.target).find('[name=nomalbum]').val());
+    Meteor.call('InsertDefinitive', ImageTab, $(e.target).find('[name=nomalbum]').val(),'1',Meteor.userId());
       
-    }
+    
   },
   'keyup .nomdelalbum' : function(e){
     e.preventDefault();
