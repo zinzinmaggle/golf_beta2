81dc5236d9cfdede724dfee13132d0643a2eb171
na-ji <naji.demolitionman@gmail.com>
2015-06-05 10:49:49 +0200

    Update profil

--- client/templates/Profil/profile_submit.js
+++ client/templates/Profil/profile_submit.js
@@ -1,22 +1,22 @@
 Template.ProfileForm.events({
-	'submit form': function(e) {
+	'click paper-button#submitProfil': function(e) {
 		e.preventDefault();
 	
 		Meteor.users.update({
 			_id:Meteor.user()._id
 		}, {
 			$set: { 
-				username: $(e.target).find('[name=username]').val(),
-				firstName: $(e.target).find('[name=fname]').val(),
-				lastName: $(e.target).find('[name=lname]').val(),
+				// username: $(e.target).find('[name=username]').val(),
+				firstName: $('[name=fname]').val(),
+				lastName: $('[name=lname]').val(),
 				accountType : 'regular',
-				gender : $(e.target).find('[name=gender]').val(),
-				phone : $(e.target).find('[name=phone]').val(),
-				adresse : $(e.target).find('[name=adresse]').val(),
-				codePostal : $(e.target).find('[name=dpt]').val(),
-				ville : $(e.target).find('[name=ville]').val(),
-				nationalite : $(e.target).find('[name=nationalite]').val(),
-				handicape : $(e.target).find('[name=hdc]').val(),
+				gender : $('#gender')[0].selectedItem.label,
+				phone : $('[name=phone]').val(),
+				adresse : $('[name=adresse]').val(),
+				codePostal : $('[name=dpt]').val(),
+				ville : $('[name=ville]').val(),
+				nationalite : $('[name=nationalite]').val(),
+				handicape : $('[name=hdc]').val(),
 			}
 		});
 
